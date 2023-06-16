1492589486e4dc9c5ba8c3f23dc3fc4c489419c6
I Fero I <afonsofariatech@gmail.com>
2023-06-17 00:05:16 +0100

    model

---
+++ BusinessLogic/Models/ViewEventoModel.cs
@@ -0,0 +1,22 @@
+﻿namespace BusinessLogic.Models;
+
+public class ViewEventoModel
+{
+    public string Nome { get; set; } = null!;
+
+    public DateOnly Data { get; set; }
+
+    public string Hora { get; set; } = null!;
+
+    public string Local { get; set; } = null!;
+
+    public string? Descricao { get; set; }
+
+    public string? Categoria { get; set; } = null!;
+
+    public int Capacidade { get; set; }
+    
+    public int NumeroAtidades { get; set; }
+    
+    public double IngressoBarato { get; set; }
+}
