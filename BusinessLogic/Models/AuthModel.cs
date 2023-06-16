f9d1a8fe0dfdcbe9d03812541af37ea93e92af94
I Fero I <afonsofariatech@gmail.com>
2023-06-16 21:31:27 +0100

    Get Role

--- BusinessLogic/Models/AuthModel.cs
+++ BusinessLogic/Models/AuthModel.cs
@@ -5,5 +5,5 @@ public class AuthModel
     public string Username { get; set; } = null!;
     public string Password { get; set; } = null!;
     public Guid Id { get; set; }
-    public string Tipo { get; set; }
+    public string Tipo { get; set; } = null!;
 }
