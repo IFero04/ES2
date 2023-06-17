86803770467f02e8ac622e311dbe0bb77ae8dbd1
I Fero I <afonsofariatech@gmail.com>
2023-06-17 12:49:34 +0100

    Eventos Inscritos

--- BusinessLogic/Models/GetInscricaoEvento.cs
+++ BusinessLogic/Models/GetInscricaoEvento.cs
@@ -1,6 +1,9 @@
-﻿namespace BusinessLogic.Models;
+﻿using BusinessLogic.Entities;
+
+namespace BusinessLogic.Models;
 
 public class GetInscricaoEvento
 {
     public List<Guid>? IdEventos { get; set; }
+    public List<IngressoDetalheModel> Ingressos { get; set; }
 }
