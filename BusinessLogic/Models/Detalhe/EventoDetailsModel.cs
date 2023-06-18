b197d5c34a9179a722b565a26f2f687d1a2cdf40
I Fero I <afonsofariatech@gmail.com>
2023-06-18 22:53:59 +0100

    Paricipantes Inscritos

--- BusinessLogic/Models/Detalhe/EventoDetailsModel.cs
+++ BusinessLogic/Models/Detalhe/EventoDetailsModel.cs
@@ -42,7 +42,7 @@ public class EventoDetailsModel
             return 0.0;
         }
     }
-    
-    public int ParticipantesInscritos { get; set; }
-   
+
+    public int ParticipantesInscritos => Ingressos?.Count ?? 0;
+
 }
