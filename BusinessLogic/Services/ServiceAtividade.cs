fcf7dd5b3a30292f98ea8cf8bf1dac87db5bfff0
Sonia227 <106330015+Sonia227@users.noreply.github.com>
2023-06-18 10:11:26 +0100

    AtividadeService

---
+++ BusinessLogic/Services/ServiceAtividade.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLogic.Services;
+
+public interface IAtividadeService
+{
+    Task<bool> RemoverAtividade(Guid id);
+}
+
+public class ServiceAtividade : IAtividadeService
+{
+    _httpClient = httpClient;
+}
